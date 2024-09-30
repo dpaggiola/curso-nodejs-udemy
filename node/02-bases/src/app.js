cd77748d96103fe764589d2293630c27aff5abf1
Daniel Paggiola <dpaggiola@gmail.com>
2024-09-30 17:10:49 -0300

    Class 27

--- node/02-bases/src/app.js
+++ node/02-bases/src/app.js
@@ -1,4 +1,16 @@
 
-const { emailTemplate } = require('./js-foundation/01-template');
+// const { emailTemplate } = require('./js-foundation/01-template');
+// require('./js-foundation/02-destructuring');
+const { getUserById } = require('./js-foundation/03-callbacks');
 
-console.log(emailTemplate);
+
+const id = 3;
+
+getUserById(id, function (error, user) {
+  if (error) {
+    throw new Error(error);
+  }
+
+  console.log(user);
+});
+
---
+++ node/02-bases/src/js-foundation/03-callbacks.js
@@ -0,0 +1,27 @@
+const users = [
+  {
+    id: 1,
+    name: 'John Doe',
+  },
+  {
+    id: 2,
+    name: 'Jane Doe',
+  },
+];
+
+
+function getUserById(id, callback) {
+  const user = users.find(function (user) {
+    return user.id === id;
+  });
+
+  if (!user) {
+    return callback(`USUARIO ${id} NO ENCONTRADO`);
+  }
+
+  return callback(null, user);
+}
+
+module.exports = {
+  getUserById,
+}
