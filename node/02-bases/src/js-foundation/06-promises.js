05859236b40f8b37c2d0d3f7b6df04469c55e826
Daniel Paggiola <dpaggiola@gmail.com>
2024-09-30 23:42:45 -0300

    Section 3 done

--- node/02-bases/src/js-foundation/06-promises.js
+++ node/02-bases/src/js-foundation/06-promises.js
@@ -1,10 +1,21 @@
-const getPokemonById = (id) => {
+const { http } = require('../plugins');
+
+
+const getPokemonById = async(id) => {
   const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
 
-  return fetch(url)
-    .then((response) => response.json())
-    // .then(() => { throw new Error('Pokemon no existe')})
-    .then((pokemon) => pokemon.name);
-};
+  const pokemon = await http.get(url);
+
+  // const resp = await fetch(url);
+  // const pokemon = await resp.json();
+
+  return pokemon.name;
+}
+
+//   return fetch(url)
+//     .then((response) => response.json())
+//     // .then(() => { throw new Error('Pokemon no existe')})
+//     .then((pokemon) => pokemon.name);
+// };
 
 module.exports = getPokemonById;
