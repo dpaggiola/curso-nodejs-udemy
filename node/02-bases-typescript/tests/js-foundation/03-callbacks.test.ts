f3dc57e77fd89b96682ba53b131ac7632eecd466
Daniel Paggiola <dpaggiola@gmail.com>
2024-10-18 15:32:14 -0300

    Class 63

--- node/02-bases-typescript/tests/js-foundation/03-callbacks.test.ts
+++ node/02-bases-typescript/tests/js-foundation/03-callbacks.test.ts
@@ -12,4 +12,18 @@ describe('js-foundation/03-callbacks.ts', () => {
             done();
         });
     });
+
+    test('getUserById should return John Doe', (done) => {
+        const id = 1;
+
+        getUserById(id, (err, user) => {
+            expect(err).toBeUndefined();
+            expect(user).toEqual({
+                id: 1,
+                name: 'John Doe',
+              });
+        });
+
+        done();
+    });
 });
